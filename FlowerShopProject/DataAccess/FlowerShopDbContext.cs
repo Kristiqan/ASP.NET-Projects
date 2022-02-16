863267d1af6d0a27d7d0ede2a332500e956fec28
Kristiqan <92256441+Kristiqan@users.noreply.github.com>
2022-02-16 20:20:52 +0200

    Update FlowerShopDbContext.cs

--- FlowerShopProject/DataAccess/FlowerShopDbContext.cs
+++ FlowerShopProject/DataAccess/FlowerShopDbContext.cs
@@ -1,10 +1,23 @@
 ﻿namespace FlowerShopProject.DataAccess
 {
+    using FlowerShopProject.Data;
     using Microsoft.EntityFrameworkCore;
 
 
     public class FlowerShopDbContext : DbContext
     {
+        
+        public FlowerShopDbContext(DbContextOptions<FlowerShopDbContext> options) : base(options)
+        {
 
+        }
+        
+        
+        
+        
+        
+        public DbSet<Flower> Flowers { get; set; }
+
+        
     }
 }
